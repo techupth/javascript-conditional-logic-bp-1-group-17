eaf8ce5195d774aa4fdb73ebceff0305f7e5d4dd
mosigaaa <mosiga2008@gmail.com>
2023-10-12 13:26:50 +0700

    "Feat: Complete assignment by Moss"

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,13 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+if (lightBulbStatus === "On"){
+    console.log("Light bulb is On.")
+} else if(lightBulbStatus === "Broken"){
+    console.log("Light bulb is Broken.")
+} else if(lightBulbStatus === "Off"){
+    console.log("Light bulb is Off.")
+} 
+  else{
+    console.log("Please choose the correct input (On/Off/Broken)")
+}
